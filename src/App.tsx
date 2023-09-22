9036a01851f7473f04435afa73bbcdb4efcea1bf
SouthViking <seb.toro.severino@gmail.com>
2023-09-22 11:31:56 -0300

    feat: displaying toast on server connection failure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
 import LoginForm from './pages/login/form';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -12,6 +14,20 @@ function App() {
             <Route path='*' element={<LoginForm />} />
           </Routes>
         </BrowserRouter>
+
+        <ToastContainer
+            position="top-center"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+        />
+
       </div>
     </div>
   );
--- src/pages/login/form.tsx
+++ src/pages/login/form.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom'
 import useWebSocket from 'react-use-websocket';
 import { Button, TextField } from '@mui/material';
@@ -30,17 +31,21 @@ export default function LoginForm () {
     React.useEffect(() => {
         if (lastMessage !== null) {
             const data = JSON.parse(lastMessage.data);
-            console.log(data);
-            if (data.type === 0 && data.success) {
-                navigate('/chat-room');
+            // TODO: Use same enum as used in the backend.
+            if (data.type === 0) {
+                if (data.success) {
+                    navigate('/chat-room');
+                } else {
+                    toast.error('Ups! There has been an internal error during the connection with the server.');
+                }
             }
-
         }
-    }, [lastMessage]);
+
+    }, [lastMessage, navigate]);
 
 
     return <>
-        <h3>💬 Enter your name to join the chat room</h3>
+        <h1>💬 Enter your name to join the chat room</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
             <TextField
                 sx={{ input: { color: 'white' } }}
